e6323a9b690b99dac05ce2d62a2ec9826e3fc6ae
Christopher Soriano <chris.soriano@gmail.com>
2022-12-22 11:44:37 -0800

    Update __init__.py
    Update from https://github.com/provostm/face-morphing-multiple-images/blob/main/code/__init__.py

--- code/__init__.py
+++ code/__init__.py
@@ -19,15 +19,35 @@ def doMorphing(img1, img2, duration, frame_rate, output):
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--img1", required=True, help="The First Image")
-	parser.add_argument("--img2", required=True, help="The Second Image")
+	parser.add_argument("--img1", help="The First Image")
+	parser.add_argument("--img2", help="The Second Image")
+	parser.add_argument("--folder", help="The folder with all images")
 	parser.add_argument("--duration", type=int, default=5, help="The duration")
 	parser.add_argument("--frame", type=int, default=20, help="The frameame Rate")
 	parser.add_argument("--output", help="Output Video Path")
+	parser.add_argument("--tmpfolder", default="tmp_videos/", help="The temporary folder to store intermediate videos")
 	args = parser.parse_args()
 
-	image1 = cv2.imread(args.img1)
-	image2 = cv2.imread(args.img2)
-
-	doMorphing(image1, image2, args.duration, args.frame, args.output)
-	
+	if(args.img1 and args.img2):
+		img1 = cv2.imread(args.img1)
+		img2 = cv2.imread(args.img2)
+		doMorphing(img1, img2, args.duration, args.frame, args.output)
+    
+	if(args.folder):
+		imgFolder = args.folder
+		listImg = os.listdir(imgFolder)
+        
+		outputList = []
+		for i in range(0, len(listImg)-1):
+			print("on traite le morphing des images "+listImg[i]+" et "+ listImg[i+1])
+			img1 = cv2.imread(os.path.join(imgFolder,listImg[i]))
+			img2 = cv2.imread(os.path.join(imgFolder, listImg[i+1]))
+			doMorphing(img1, img2, args.duration, args.frame, args.tmpfolder + str(i)+"_"+args.output)
+			outputList.append("file '"+ args.tmpfolder + str(i)+"_"+args.output+"'")
+		f=open('imageslist.txt','w')
+		s1='\n'.join(outputList)
+		f.write(s1)
+		f.close()
+		
+    # merge all the temporary videos into one
+	os.system('ffmpeg -f concat -safe 0 -i imageslist.txt -c copy ' + args.output)
